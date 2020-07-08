b5227d08c3cd328c6e3ca3ea24ad476ed1575825
1446450047 <1446450047@qq.com>
2020-07-08 17:18:06 +0800

    type切换完成

--- src/components/Add/Type.vue
+++ src/components/Add/Type.vue
@@ -1,17 +1,30 @@
 <template>
     <div class="type">
         <ul>
-            <li class="selected">支出</li>
-            <li>收入</li>
+            <li :class="type === '-' && 'selected' " @click="selectType('-')">支出</li>
+            <li :class="type === '+' && 'selected' " @click="selectType('+')">收入</li>
         </ul>
     </div>
 
 </template>
 
 <script lang="ts">
-    export default {
-        name: "Type"
-    };
+    import {Component} from "vue-property-decorator";
+    import Vue from 'vue';
+
+    @Component
+   export default class Type extends Vue{
+       type =  "-";
+       selectType(type: string){
+           if(type !== "-" && type !== "+"){
+               throw new Error("type is not unknown")
+           }
+           this.type = type;
+       }
+       mounted(): void {
+           console.log(this.type);
+       }
+    }
 </script>
 
 <style scoped lang="scss">
