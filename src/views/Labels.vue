89b0fb67259a1636d27ac2e8076459cf91ed5dff
1446450047 <1446450047@qq.com>
2020-07-09 22:16:35 +0800

    label页面的HTML完成

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,24 @@
 <template>
     <div>
         <Layout>
-            <p>labels</p>
+            <ul>
+                <li>
+                   <span> 衣</span>
+                    <Icon name="right"/>
+                </li>
+                <li>
+                   <span> 食</span>
+                    <Icon name="right"/>
+                </li>
+                <li>
+                   <span> 住</span>
+                    <Icon name="right"/>
+                </li>
+                <li>
+                   <span> 行</span>
+                    <Icon name="right"/>
+                </li>
+            </ul>
         </Layout>
     </div>
 </template>
