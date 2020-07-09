9eebe87f270bb2abd5c0913773103dfab6bf6d07
1446450047 <1446450047@qq.com>
2020-07-09 11:31:52 +0800

    eslint

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 // const path = require("path");
-const path =  import("path");
+/* eslint-disable */
+const path =  require("path");
 //import path from "path"
 module.exports = {
     lintOnSave: false,
     chainWebpack: config => {
+
         const dir = path.resolve(__dirname, "src/assets/icons");
 
         config.module
@@ -13,7 +15,8 @@ module.exports = {
             .use("svg-sprite-loader").loader("svg-sprite-loader").options({extract: false}).end();
         // .use("svgo-loader").loader('svgo-loader')
         // .tap(options => ({...options, plugins: [{removeAttrs: {attrs: "fill"}}]})).end();
-        config.plugin("svg-sprite").use(import("svg-sprite-loader/plugin"), [{plainSprite: true}] || []);
+        /* eslint-disable */
+        config.plugin("svg-sprite").use(require("svg-sprite-loader/plugin"), [{plainSprite: true}] || []);
         config.module.rule("svg").exclude.add(dir); // 其他 svg loader 排除 icons 目录
     }
 };
